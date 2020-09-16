4ff192ff6e864d1580dd3bc7aa539eba1c366cb2
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-09-16 02:03:37 +0200

    chore: fix tests run script

--- tests/run.sh
+++ tests/run.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 set -e
 
-echo | bazel run //:blackjack
+# CI runs on ubuntu-latest
+target=x86_64-unknown-linux-gnu
+
+echo | bazel run //:blackjack -- --target="$target"
 bazel build //:all
 
 pushd tests/popular_crates
-echo | bazel run //:blackjack
+echo | bazel run //:blackjack -- --target="$target"
 bazel build //:crates
 popd
 
 pushd tests/linkc
-echo | bazel run //:blackjack
+echo | bazel run //:blackjack -- --target="$target"
 bazel run //:main
 popd
 
 pushd tests/workspace
-echo | bazel run //:blackjack
+echo | bazel run //:blackjack -- --target="$target"
 bazel run //crate1
 bazel run //crate2
 popd
